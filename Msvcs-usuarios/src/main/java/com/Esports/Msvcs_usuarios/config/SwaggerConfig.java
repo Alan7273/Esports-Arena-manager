a25b9705b36b4b3d6c1cab35ebeaa3307ae8f587
alan7273 <ala.cubillos@duocuc.cl>
2026-06-12 20:58:56 -0400

    Implementacion de Swagger por alan

--- Msvcs-usuarios/src/main/java/com/Esports/Msvcs_usuarios/config/SwaggerConfig.java
+++ Msvcs-usuarios/src/main/java/com/Esports/Msvcs_usuarios/config/SwaggerConfig.java
@@ -11,8 +11,8 @@ public class SwaggerConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("API Esports - Juegos")
+                        .title("API Esports-Arena-Manager - Usuarios")
                         .version("1.0")
-                        .description("Documentación de la API del microservicio de Juegos"));
+                        .description("Documentación de la API del microservicio de Usuarios"));
     }
 }
