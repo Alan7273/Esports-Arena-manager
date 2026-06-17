85c069f5c1e258cf4786d18a9f566a0f7b5e40d3
Alan7273 <ala.cubillos@duocuc.cl>
2026-06-17 10:06:46 -0400

    Creacion de microservicio de Users para logins y eureka

---
+++ Msvcs-Users/src/main/java/com/esports/msvcs_user/MsvcsUsersApplication.java
@@ -0,0 +1,13 @@
+package com.esports.msvcs_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcsUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsvcsUsersApplication.class, args);
+    }
+
+}
---
+++ Msvcs-Users/src/test/java/com/esports/msvcs_user/MsvcsUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esports.msvcs_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcsUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
